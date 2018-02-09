7e3eff97e609d741a5328c38fbfe9482995c5a16
ahutsunshine <ahutsunshine@gmail.com>
2018-02-09 10:02:53 +0800

    add get greate sum of sub array algorithm

---
+++ src/com/algrithm/sword/offer/Interview31_GreatestSubArraySum.java
@@ -0,0 +1,30 @@
+package com.algrithm.sword.offer;
+
+/**
+ * @author Kevin Yao
+ */
+public class Interview31_GreatestSubArraySum {
+    public static void main(String[] args) {
+        int[] a = {1, -2, 3, 10, -4, 7, 2, -5};
+        System.out.println(getGreatestSubArraySum(a));
+    }
+
+    private static int getGreatestSubArraySum(int[] a) {
+        if (a == null || a.length == 0) {
+            return Integer.MIN_VALUE;
+        }
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int val : a) {
+            if (sum < 0) {
+                sum = val;
+            } else {
+                sum += val;
+            }
+            if (sum > max) {
+                max=sum;
+            }
+        }
+        return max;
+    }
+}
