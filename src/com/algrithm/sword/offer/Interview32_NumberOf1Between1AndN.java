e44ca04e8a1b6e42ee6f61d2b89af79512811564
ahutsunshine <ahutsunshine@gmail.com>
2018-02-10 17:14:23 +0800

    add logn number of 1

--- src/com/algrithm/sword/offer/Interview32_NumberOf1Between1AndN.java
+++ src/com/algrithm/sword/offer/Interview32_NumberOf1Between1AndN.java
@@ -2,11 +2,36 @@ package com.algrithm.sword.offer;
 
 public class Interview32_NumberOf1Between1AndN {
     public static void main(String[] args) {
-        int n =12;
-        System.out.println(NumberOf1Between1AndN_String(n));
+        int n = 1000;
+        int result1 = numberOf1Between1AndN_String(n);
+        int result2 = numberOf1Between1AndN(n);
+
+        assert result1 == result2;
+        System.out.println(result1);
+
+
     }
 
-    private static int NumberOf1Between1AndN_String(int n) {
+    private static int numberOf1Between1AndN(int n) {
+        int base = 1;
+        int round = n;
+        int count = 0;
+        int weight;
+        while (round > 0) {
+            weight = round % 10;
+            round /= 10;
+            if (weight == 0) {
+                count += round * base;
+            } else if (weight == 1) {
+                count += round * base + (n % base) + 1;
+            } else {
+                count += round * base + base;
+            }
+        }
+        return count;
+    }
+
+    private static int numberOf1Between1AndN_String(int n) {
         int count = 0;
         while (n > 0) {
             count += NumberOf1_String(n);
