4f54dc362585a8dd5f749ec6556ee535aed6602e
ahutsunshine <ahutsunshine@gmail.com>
2018-02-11 15:22:24 +0800

    add find first common node algorithm

---
+++ src/com/algorithm/entity/ListNode.java
@@ -0,0 +1,13 @@
+package com.algorithm.entity;
+
+/**
+ * @author Kevin Yao
+ */
+public class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+}
---
+++ src/com/algorithm/sword/offer/Interview_36_FindFirstCommonNode.java
@@ -0,0 +1,60 @@
+package com.algorithm.sword.offer;
+
+import com.algorithm.entity.ListNode;
+
+/**
+ * @author Kevin Yao
+ */
+public class Interview_36_FindFirstCommonNode {
+    public static void main(String[] args) {
+        ListNode node1 = new ListNode(1);
+        ListNode node2 = new ListNode(2);
+        ListNode node3 = new ListNode(3);
+        ListNode node4 = new ListNode(4);
+        ListNode node5 = new ListNode(5);
+        ListNode node6 = new ListNode(6);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        node6.next = node3;
+        ListNode common = findFirstCommonNode(node1, node6);
+        System.out.println(common.val);
+
+    }
+
+    private static ListNode findFirstCommonNode(ListNode pHead1, ListNode pHead2) {
+        if (pHead1 == null || pHead2 == null) {
+            return null;
+        }
+        int length1 = getLength(pHead1);
+        int length2 = getLength(pHead2);
+        int dif = length2 - length1;
+        ListNode pNodeShort = pHead1;
+        ListNode pNodeLong = pHead2;
+        if (length1 > length2) {
+            pNodeShort = pHead2;
+            pNodeLong = pHead1;
+            dif = length1 - length2;
+        }
+        while (dif > 0) {
+            pNodeLong = pNodeLong.next;
+            dif--;
+        }
+        while (pNodeShort != null && pNodeLong != null && pNodeShort != pNodeLong) {
+            pNodeShort = pNodeShort.next;
+            pNodeLong = pNodeLong.next;
+        }
+        return pNodeShort;
+
+    }
+
+    private static int getLength(ListNode head) {
+        int length = 0;
+        while (head != null) {
+            length++;
+            head = head.next;
+        }
+        return length;
+    }
+}
