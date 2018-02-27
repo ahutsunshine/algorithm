001f679b92a8eb7d2de3ac347d637a6ba16ab45e
ahutsunshine <ahutsunshine@gmail.com>
2018-02-27 18:55:01 +0800

    add enum singleton algorithm

---
+++ src/com/algorithm/singleton/SingleEnum.java
@@ -0,0 +1,12 @@
+package com.algorithm.singleton;
+
+/**
+ * @author Kevin Yao
+ */
+public enum  SingleEnum {
+    SINGLETON_INSTANCE;
+
+    public void print() {
+        System.out.println("Enum singleton.");
+    }
+}
