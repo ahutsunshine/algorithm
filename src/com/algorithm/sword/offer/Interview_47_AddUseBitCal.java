405a6a1effae6bb2a34caaf3ffd58aa9e4c10b1f
ahutsunshine <ahutsunshine@gmail.com>
2018-08-28 15:53:01 +0800

    add bit cal

---
+++ src/com/algorithm/sword/offer/Interview_47_AddUseBitCal.java
@@ -0,0 +1,19 @@
+package com.algorithm.sword.offer;
+
+public class Interview_47_AddUseBitCal {
+    public static void main(String[] args) {
+        int num1 = 19, num2 = 29;
+        System.out.println(add(num1, num2));
+    }
+
+    private static int add(int num1, int num2) {
+        int sum, carry;
+        do {
+            sum = num1 ^ num2;
+            carry = (num1 & num2) << 1;
+            num1 = sum;
+            num2 = carry;
+        } while (num2 != 0);
+        return num1;
+    }
+}
