9f332d3e54ade72ae9de839c3fd141359c8be237
ahutsunshine <ahutsunshine@gmail.com>
2018-11-09 19:04:38 +0800

    add lowest common ansector for bst

---
+++ src/com/algorithm/leetcode/tree/_235_LCAOfBinarySearchTree.java
@@ -0,0 +1,62 @@
+package com.algorithm.leetcode.tree;
+
+import java.util.List;
+
+/**
+ * Given a binary search tree (BST), find the lowest common ancestor (LCA) of two given nodes in the BST.
+ *
+ * According to the definition of LCA on Wikipedia: “The lowest common ancestor is defined between two nodes p and q as the lowest node in T that has both p and q as descendants (where we allow a node to be a descendant of itself).”
+ *
+ * Given binary search tree:  root = [6,2,8,0,4,7,9,null,null,3,5]
+ *
+ *         _______6______
+ *        /              \
+ *     ___2__          ___8__
+ *    /      \        /      \
+ *    0      _4       7       9
+ *          /  \
+ *          3   5
+ * Example 1:
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+ * Output: 6
+ * Explanation: The LCA of nodes 2 and 8 is 6.
+ * Example 2:
+ *
+ * Input: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+ * Output: 2
+ * Explanation: The LCA of nodes 2 and 4 is 2, since a node can be a descendant of itself
+ *              according to the LCA definition.
+ */
+
+public class _235_LCAOfBinarySearchTree {
+    public static void main(String[] args) {
+        TreeNode p = new TreeNode(6);
+        TreeNode p1 = new TreeNode(2);
+        TreeNode p2 = new TreeNode(8);
+        TreeNode p3 = new TreeNode(0);
+        TreeNode p4 = new TreeNode(4);
+        TreeNode p5 = new TreeNode(7);
+        TreeNode p6 = new TreeNode(9);
+        p.left = p1;
+        p.right = p2;
+        p1.left = p3;
+        p1.right = p4;
+        p2.left = p5;
+        p2.right = p6;
+
+        TreeNode lca = lowestCommonAncestor(p, p3, p4);
+        assert (lca.val == 2);
+    }
+
+    public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root.val > p.val && root.val > q.val){
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(root.val < p.val && root.val < q.val){
+            return lowestCommonAncestor(root.right, p, q);
+        }else{
+            return root;
+        }
+    }
+}
