9f39e08bab8243062978e5066d9cee5041f26e1a
ahutsunshine <ahutsunshine@gmail.com>
2018-12-28 15:48:03 +0800

    implement trie with hashmap

---
+++ src/com/algorithm/leetcode/tree/_208_Trie.java
@@ -0,0 +1,87 @@
+package com.algorithm.leetcode.tree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Implement a trie with insert, search, and startsWith methods.
+ *
+ * Example:
+ *
+ * Trie trie = new Trie();
+ *
+ * trie.insert("apple");
+ * trie.search("apple");   // returns true
+ * trie.search("app");     // returns false
+ * trie.startsWith("app"); // returns true
+ * trie.insert("app");
+ * trie.search("app");     // returns true
+ * Note:
+ *
+ * You may assume that all inputs are consist of lowercase letters a-z.
+ * All inputs are guaranteed to be non-empty strings.
+ */
+public class _208_Trie {
+    TrieNode root;
+
+    public _208_Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        if (word == null) return;
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                node.children.put(c, new TrieNode());
+            }
+            node = node.children.get(c);
+        }
+        node.isWord = true;
+    }
+
+    public boolean search(String word) {
+        if (word == null) return false;
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                return false;
+            }
+            node = node.children.get(c);
+        }
+        return node.isWord;
+    }
+
+    public boolean startsWith(String prefix) {
+        if(prefix == null) return false;
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (!node.children.containsKey(c)) {
+                return false;
+            }
+            node = node.children.get(c);
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        _208_Trie trie = new _208_Trie();
+
+        trie.insert("apple");
+        System.out.println(trie.search("apple"));   // returns true
+        System.out.println(trie.search("app"));     // returns false
+        System.out.println(trie.startsWith("app")); // returns true
+        trie.insert("app");
+        System.out.println(trie.search("app"));     // returns true
+    }
+
+}
+
+class TrieNode {
+    boolean isWord = false;
+    Map<Character, TrieNode> children;
+
+    public TrieNode() {
+        children = new HashMap<>();
+    }
+}
